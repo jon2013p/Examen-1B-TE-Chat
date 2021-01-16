68e675a05b84e411273e94bd00f40ca09f28ca96
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2021-01-15 19:31:33 -0500

    pagina y configuracion de registro de usuarios

--- chat/src/app/register/register.page.ts
+++ chat/src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AuthenticateService } from '../services/authentication.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email es obligatorio.' },
+      { type: 'pattern', message: 'Por favor, ingresa un email válido.' }
+    ],
+    'password': [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      { type: 'minlength', message: 'Por favor, ingresa una contraseña de al menos 5 caracteres' }
+    ]
+  };
+
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthenticateService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
   }
+
+  tryRegister(value) {
+    this.authService.registerUser(value)
+      .then(res => {
+        console.log(res);
+        this.errorMessage = "";
+        this.successMessage = "Tu cuenta ha sido creada satisfactoriamente, por favor inicia sesión";
+      }, err => {
+        console.log(err);
+        this.errorMessage = err.message;
+        this.successMessage = "";
+      })
+  }
+
+  goLoginPage() {
+    this.navCtrl.navigateBack('');
+  }
+
 
 }
